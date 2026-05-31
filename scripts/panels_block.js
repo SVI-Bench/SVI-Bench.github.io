6c56ea3ad83160126e39ca8960a863ca3d19d6c5
Seongsu Ha <sha17@oprime.ib>
2026-05-31 12:51:19 -0400

    Mobile: T3 static thumbnails, T7/T8 low-res videos, ask video height 150px

--- scripts/panels_block.js
+++ scripts/panels_block.js
@@ -251,7 +251,17 @@ function T3Panel(_ref) {
         transition: 'opacity 150ms, box-shadow 150ms',
         boxShadow: isSelected ? "0 0 12px ".concat(edgeGlow) : 'none'
       }
-    }, /*#__PURE__*/React.createElement("video", {
+    }, window.innerWidth <= 768 ? /*#__PURE__*/React.createElement("img", {
+      src: th.video.replace('.mp4', '_thumb.jpg'),
+      alt: th.caption,
+      style: {
+        width: '100%',
+        height: '100%',
+        objectFit: 'cover',
+        display: 'block',
+        filter: isSelected ? 'none' : 'saturate(0.7)'
+      }
+    }) : /*#__PURE__*/React.createElement("video", {
       src: th.video,
       muted: true,
       playsInline: true,
